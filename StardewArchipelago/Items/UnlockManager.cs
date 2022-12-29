068e9d4f8fda312073417f803462c87989dc8f4c
Alex Gilbert <alexgilbert@yahoo.com>
2022-12-29 17:17:58 -0500

    - Ability to receive progressive tools

--- StardewArchipelago/Items/UnlockManager.cs
+++ StardewArchipelago/Items/UnlockManager.cs
@@ -4,9 +4,11 @@ using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
 using Netcode;
 using StardewValley;
 using StardewValley.Objects;
+using StardewValley.Tools;
 
 namespace StardewArchipelago.Items
 {
@@ -19,6 +21,7 @@ namespace StardewArchipelago.Items
             _unlockables = new Dictionary<string, Action<int>>();
             RegisterCommunityCenterRepairs();
             RegisterPlayerImprovement();
+            RegisterProgressiveTools();
         }
 
         public bool IsUnlock(string unlockName)
@@ -39,6 +42,20 @@ namespace StardewArchipelago.Items
             _unlockables.Add("Minecarts Repair", (_) => RepairMinecarts());
             _unlockables.Add("Bus Repair", (_) => RepairBus());
             // _unlockables.Add("Movie Theater", BuildMovieTheater);
+        }
+
+        private void RegisterPlayerImprovement()
+        {
+            _unlockables.Add("Progressive Backpack", SetBackPackLevel);
+        }
+
+        private void RegisterProgressiveTools()
+        {
+            _unlockables.Add("Progressive Axe", ReceiveProgressiveAxe);
+            _unlockables.Add("Progressive Pickaxe", ReceiveProgressivePickaxe);
+            _unlockables.Add("Progressive Hoe", ReceiveProgressiveHoe);
+            _unlockables.Add("Progressive Watering Can", ReceiveProgressiveWateringCan);
+            _unlockables.Add("Progressive Trash Can", ReceiveProgressiveTrashCan);
         }
 
         private void RepairBridge()
@@ -76,11 +93,6 @@ namespace StardewArchipelago.Items
             Game1.player.mailForTomorrow.Add(mailTitle + "%&NL&%");
         }
 
-        private void RegisterPlayerImprovement()
-        {
-            _unlockables.Add("Progressive Backpack", (numberReceived) => SetBackPackLevel(numberReceived));
-        }
-
         private void SetBackPackLevel(int level)
         {
             var previousMaxItems = Game1.player.MaxItems;
@@ -106,6 +118,58 @@ namespace StardewArchipelago.Items
                     Game1.player.Items.Add(null);
                 }
                 Game1.player.holdUpItemThenMessage(new SpecialItem(99, level == 1 ? backpack1Name : backpack2Name));
+            }
+        }
+
+        private void ReceiveProgressiveAxe(int numberReceived)
+        {
+            ReceiveProgressiveTool(numberReceived, () => new Axe(), "Axe");
+        }
+
+        private void ReceiveProgressivePickaxe(int numberReceived)
+        {
+            ReceiveProgressiveTool(numberReceived, () => new Pickaxe(), "Pickaxe");
+        }
+
+        private void ReceiveProgressiveHoe(int numberReceived)
+        {
+            ReceiveProgressiveTool(numberReceived, () => new Hoe(), "Hoe");
+        }
+
+        private void ReceiveProgressiveWateringCan(int numberReceived)
+        {
+            ReceiveProgressiveTool(numberReceived, () => new WateringCan(), "Watering Can");
+        }
+
+        private void ReceiveProgressiveTrashCan(int numberReceived)
+        {
+            ReceiveProgressiveTool(numberReceived, () => new GenericTool(), "Trash Can");
+        }
+
+        private void ReceiveProgressiveTool(int numberReceived, Func<Tool> toolCreationFunction, string toolGenericName)
+        {
+            var player = Game1.player;
+            var playerAxes = new List<Tool>();
+            foreach (Item playerItem in player.Items)
+            {
+                if (playerItem != null && playerItem is Tool && playerItem.Name.Contains(toolGenericName))
+                {
+                    Game1.player.removeItemFromInventory(playerItem);
+                }
+            }
+
+            var newTool = toolCreationFunction();
+            newTool.UpgradeLevel = numberReceived;
+
+            Game1.player.holdUpItemThenMessage(newTool);
+
+            if (newTool is GenericTool)
+            {
+                Game1.player.trashCanLevel = numberReceived;
+            }
+            else
+            {
+                Game1.player.addItemByMenuIfNecessary(newTool);
             }
         }
     }
