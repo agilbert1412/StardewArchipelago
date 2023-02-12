ccd1c0d29371067215ca5e49fd6484b7c679209b
Alex Gilbert <alexgilbert@yahoo.com>
2023-02-12 16:03:33 -0500

    - missing file for fishsanity

---
+++ StardewArchipelago/Locations/CodeInjections/FishingInjections.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using StardewArchipelago.Archipelago;
+using StardewArchipelago.Items;
+using StardewArchipelago.Stardew;
+using StardewModdingAPI;
+using StardewValley;
+using StardewValley.Locations;
+using StardewValley.Objects;
+using StardewValley.Tools;
+
+namespace StardewArchipelago.Locations.CodeInjections
+{
+    public static class FishingInjections
+    {
+        private const string FISHSANITY_PREFIX = "Fishsanity: ";
+
+        private static IMonitor _monitor;
+        private static IModHelper _modHelper;
+        private static ArchipelagoClient _archipelago;
+        private static LocationChecker _locationChecker;
+        private static StardewItemManager _itemManager;
+
+        public static void Initialize(IMonitor monitor, IModHelper modHelper, ArchipelagoClient archipelago, LocationChecker locationChecker, StardewItemManager itemManager)
+        {
+            _monitor = monitor;
+            _modHelper = modHelper;
+            _archipelago = archipelago;
+            _locationChecker = locationChecker;
+            _itemManager = itemManager;
+        }
+        
+        public static void CaughtFish_Fishsanity_Postfix(Farmer __instance, int index, int size, bool from_fish_pond, int numberCaught, ref bool __result)
+        {
+            try
+            {
+                if (from_fish_pond || (index >= 167 && index < 173) || !_itemManager.ItemExists(index))
+                {
+                    return;
+                }
+
+                var fish = _itemManager.GetObjectById(index);
+                var fishName = fish.Name;
+                var apLocation = $"{FISHSANITY_PREFIX}{fishName}";
+                if (_archipelago.GetLocationId(apLocation) > -1)
+                {
+                    _locationChecker.AddCheckedLocation(apLocation);
+                }
+                else
+                {
+                    _monitor.Log($"Unrecognized Fishsanity Location: {fishName} [{index}]", LogLevel.Error);
+                }
+            }
+            catch (Exception ex)
+            {
+                _monitor.Log($"Failed in {nameof(CaughtFish_Fishsanity_Postfix)}:\n{ex}", LogLevel.Error);
+                return;
+            }
+        }
+    }
+}
