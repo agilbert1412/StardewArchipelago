42cf8ffc925b42cac9ec6de06998a12db82663c9
Alex Gilbert <alexgilbert@yahoo.com>
2023-06-27 14:32:48 -0400

    - Fixed fishsanity letters and stardrop stuff

--- StardewArchipelago/Archipelago/SlotData.cs
+++ StardewArchipelago/Archipelago/SlotData.cs
@@ -260,6 +260,9 @@ namespace StardewArchipelago.Archipelago
         Special = 2,
         RandomSelection = 3,
         All = 4,
+        ExcludeLegendaries = 5,
+        ExcludeHardFish = 6,
+        OnlyEasyFish = 7,
     }
 
     public enum Museumsanity
