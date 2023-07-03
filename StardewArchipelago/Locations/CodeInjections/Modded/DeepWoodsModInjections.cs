d031a9a6f26a0b5ae4c81066150844fea7c16c8c
Alex Gilbert <alexgilbert@yahoo.com>
2023-07-03 14:37:47 -0400

    - Attempt at fixing the deep woods reflection

--- StardewArchipelago/Locations/CodeInjections/Modded/DeepWoodsModInjections.cs
+++ StardewArchipelago/Locations/CodeInjections/Modded/DeepWoodsModInjections.cs
@@ -93,11 +93,10 @@ namespace StardewArchipelago.Locations.CodeInjections.Modded
             try
             {
                 var deepWoodsStateType = AccessTools.TypeByName("DeepWoodsMod.DeepWoodsStateData");
-                var lowestLevelReachedField = _helper.Reflection.GetField<int>(deepWoodsStateType, "LowestLevelReached");
+                var lowestLevelReachedField = _helper.Reflection.GetField<int>(deepWoodsStateType, "lowestLevelReached");
                 var lowestLevelReached = lowestLevelReachedField.GetValue();
-                lowestLevelReached = 10 * _archipelago.GetReceivedItemCount(WOODS_OBELISK_SIGILS);
-
-
+                lowestLevelReachedField.SetValue(10 * _archipelago.GetReceivedItemCount(WOODS_OBELISK_SIGILS));
+                
                 if (level % LEVEL_STEP != 0)
                 {
                     return;
