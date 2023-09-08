65c0bb30db07d35d8e49b37bd0bf8bc0c7163ba9
Alex Gilbert <alexgilbert@yahoo.com>
2023-09-08 12:26:05 -0400

    - Made medium debuff traps a bit more forgiving

--- StardewArchipelago/Items/Traps/TrapDifficultyBalancer.cs
+++ StardewArchipelago/Items/Traps/TrapDifficultyBalancer.cs
@@ -15,18 +15,18 @@ namespace StardewArchipelago.Items.Traps
             {TrapItemsDifficulty.NoTraps, BuffDuration.Zero},
             {TrapItemsDifficulty.Easy, BuffDuration.TenMinutes},
             {TrapItemsDifficulty.Medium, BuffDuration.HalfHour},
-            {TrapItemsDifficulty.Hard, BuffDuration.TwoHours},
-            {TrapItemsDifficulty.Hell, BuffDuration.FourHours},
+            {TrapItemsDifficulty.Hard, BuffDuration.OneHour},
+            {TrapItemsDifficulty.Hell, BuffDuration.TwoHours},
             {TrapItemsDifficulty.Nightmare, BuffDuration.WholeDay},
         };
 
         public Dictionary<TrapItemsDifficulty, BuffDuration> DefaultDebuffDurations = new()
         {
             {TrapItemsDifficulty.NoTraps, BuffDuration.Zero},
-            {TrapItemsDifficulty.Easy, BuffDuration.OneHour},
-            {TrapItemsDifficulty.Medium, BuffDuration.TwoHours},
-            {TrapItemsDifficulty.Hard, BuffDuration.FourHours},
-            {TrapItemsDifficulty.Hell, BuffDuration.WholeDay},
+            {TrapItemsDifficulty.Easy, BuffDuration.HalfHour},
+            {TrapItemsDifficulty.Medium, BuffDuration.OneHour},
+            {TrapItemsDifficulty.Hard, BuffDuration.TwoHours},
+            {TrapItemsDifficulty.Hell, BuffDuration.FourHours},
             {TrapItemsDifficulty.Nightmare, BuffDuration.WholeDay},
         };
 
