d46e9862700ab3d4fc3241faffdda90f369acc21
Alex Gilbert <alexgilbert@yahoo.com>
2023-09-17 14:53:07 -0400

    - Added some streamer names to the baby names

--- StardewArchipelago/Items/BabyBirther.cs
+++ StardewArchipelago/Items/BabyBirther.cs
@@ -18,9 +18,9 @@ namespace StardewArchipelago.Items
             "Albrekka", "Alchav", "axe_y", "beauxq", "Berserker", "Black Sliver", "blastron", "BootsinSoots", "CaitSith2", "Captain Canadia", "Celeste", "Damocles",
             "dewin", "el_", "Espeon", "eudaimonistic", "Exempt-Medic", "Farrak Kilhn", "Figment", "Fly Sniper",
             "Frazzleduck", "Heinermann", "Ishigh", "JaredWeakStrike", "Jarno", "jat2980", "Jouramie", "Justice", "Kaito Kid",
-            "KelioMZX", "KittyAshy", "Kono Tyran", "lordlou", "Magnemania", "Marech", "Mati", "Mav", "MeleneV", "mewlif", "N00byKing", "Phar",
-            "PoryGone", "ProfBytes", "RaspberrySpaceJam", "Rosalie", "Salzkorn", "Scipio", "Sneaki", "Snow", "SunnyBat",
-            "TheCondor", "toaster", "Trev", "Violet", "Yellow_Meep", "zig", "Ziktofel",
+            "KelioMZX", "KittyAshy", "Kono Tyran", "Lady Redacted", "lordlou", "Magnemania", "Marech", "Mati", "Mav", "MeleneV", "mewlif", "N00byKing", "Phar",
+            "PoryGone", "ProfBytes", "RaspberrySpaceJam", "Rosalie", "Salzkorn", "SamanthaCoffin", "Scipio", "Sneaki", "Snow", "SunnyBat",
+            "TheCondor", "toaster", "Trev", "Violet", "WhySoThirstyy", "Yellow_Meep", "zig", "Ziktofel",
         };
 
         public void SpawnNewBaby()
