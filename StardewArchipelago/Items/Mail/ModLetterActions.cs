e3094cfc4ee73f718b1b9e7b390acaee352f19d7
Witchybun <elnendil@gmail.com>
2023-11-28 22:17:26 -0600

    Shorten the pull to weapon, not the entire entry

--- StardewArchipelago/Items/Mail/ModLetterActions.cs
+++ StardewArchipelago/Items/Mail/ModLetterActions.cs
@@ -18,7 +18,7 @@ namespace StardewArchipelago.Items.Mail
         {
             Game1.playSound("parry");
             var weaponData = Game1.content.Load<Dictionary<int, string>>("Data\\weapons");
-            var id = weaponData.FirstOrDefault(x => x.Value == "Diamond Wand/A magic relic from Galdora. Teleports enemies away from the user. Effective against flyers./1/2/31/6/800/0/3/0/-1/-1/1/1/Diamond Wand");
+            var id = weaponData.FirstOrDefault(x => x.Value.Contains("Diamond Wand"));
             var diamondWand = new MeleeWeapon(id.Key);
             Game1.player.holdUpItemThenMessage(diamondWand);
             Game1.player.addItemByMenuIfNecessary(diamondWand);
