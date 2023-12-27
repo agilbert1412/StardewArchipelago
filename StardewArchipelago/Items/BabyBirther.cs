b4d13b4de6bfbbac22c945ad3490dc0ed6e28807
Alex Gilbert <alexgilbert@yahoo.com>
2023-12-27 14:04:06 -0500

    - Added Noca Baby

--- StardewArchipelago/Items/BabyBirther.cs
+++ StardewArchipelago/Items/BabyBirther.cs
@@ -16,7 +16,8 @@ namespace StardewArchipelago.Items
             "Albrekka", "Alchav", "Angela", "axe_y", "beauxq", "Berserker", "Black Sliver", "blastron", "BootsinSoots", "CaitSith2", "Captain Canadia", "Celeste", "Damocles",
             "dewin", "el_", "emmalution", "Espeon", "eudaimonistic", "Exempt-Medic", "Farrak Kilhn", "Figment", "Fly Sniper",
             "Frazzleduck", "Heinermann", "Ishigh", "JaredWeakStrike", "Jarno", "jat2980", "jennnifer", "Jouramie", "Justice", "Kaito Kid",
-            "KelioMZX", "KittyAshy", "Kono Tyran", "Lady Redacted", "lordlou", "Magnemania", "Marech", "Margot", "Mati", "Mav", "MeleneV", "mewlif", "N00byKing"/*, "NinoKito"*/, "Phar",
+            "KelioMZX", "KittyAshy", "Kono Tyran", "Lady Redacted", "lordlou", "Magnemania", "Marech", "Margot", "Mati", "Mav", "MeleneV", "mewlif",
+            "Noca", "N00byKing"/*, "NinoKito"*/, "Phar",
             "PoryGone", "ProfBytes", "RaspberrySpaceJam", "Rosalie", "Salzkorn", "SamanthaCoffin", "Scipio", "Sneaki", "Snow", "Sraw", "SunnyBat",
             "TheCondor", "toaster", "Trev", "Violet", "WhySoThirstyy", "Yellow_Meep", "zig", "Ziktofel",
         };
