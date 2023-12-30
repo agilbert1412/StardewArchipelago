e3009adef0f2d722de39aac50a9a5b6766fec830
Witchybun <elnendil@gmail.com>
2023-12-30 03:37:10 -0600

    Clean up logic patcher

--- StardewArchipelago/GameModifications/Modded/ModRandomizedLogicPatcher.cs
+++ StardewArchipelago/GameModifications/Modded/ModRandomizedLogicPatcher.cs
@@ -1,31 +1,12 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
+
 using HarmonyLib;
-using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Content;
-using Microsoft.Xna.Framework.Graphics;
 using StardewArchipelago.Archipelago;
 using StardewArchipelago.Constants;
-using StardewArchipelago.GameModifications.CodeInjections;
 using StardewArchipelago.GameModifications.CodeInjections.Modded;
-using StardewArchipelago.GameModifications.EntranceRandomizer;
-using StardewArchipelago.GameModifications.Modded;
-using StardewArchipelago.GameModifications.Seasons;
-using StardewArchipelago.GameModifications.Tooltips;
-using StardewArchipelago.Locations;
-using StardewArchipelago.Locations.CodeInjections.Vanilla.Relationship;
 using StardewArchipelago.Stardew;
-using StardewArchipelago.Stardew.NameMapping;
 using StardewModdingAPI;
 using StardewValley;
-using StardewValley.Buildings;
-using StardewValley.Events;
-using StardewValley.Locations;
 using StardewValley.Menus;
-using StardewValley.Network;
-using StardewValley.Objects;
-using Object = StardewValley.Object;
 
 namespace StardewArchipelago.GameModifications.Modded
 {
@@ -39,7 +20,8 @@ namespace StardewArchipelago.GameModifications.Modded
         {
             _harmony = harmony;
             _archipelago = archipelago;
-            JunimoShopInjections.Initialize(monitor, modHelper, archipelago, shopStockGenerator, stardewItemManager, junimoShopGenerator);
+            _stardewItemManager = stardewItemManager;
+            JunimoShopInjections.Initialize(monitor, modHelper, archipelago, shopStockGenerator, _stardewItemManager, junimoShopGenerator);
 
         }
 
