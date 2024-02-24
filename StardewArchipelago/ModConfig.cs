eaf5f147caa1063fd0e327f3602d706eb4fa033a
Alex Gilbert <alexgilbert@yahoo.com>
2024-02-24 00:21:52 -0500

    - Moved the !!letters setting into a mod config instead of a save config
    # Conflicts:
    #	StardewArchipelago/Archipelago/ChatForwarder.cs
    #	StardewArchipelago/Items/Mail/MailPatcher.cs
    #	StardewArchipelago/Serialization/ArchipelagoStateDto.cs

--- StardewArchipelago/ModConfig.cs
+++ StardewArchipelago/ModConfig.cs
@@ -7,5 +7,12 @@
         /// Jojamart sells cheaper seeds, but forces you to purchase large packs instead of one at a time
         /// </summary>
         public bool EnableSeedShopOverhaul { get; set; } = true;
+
+        /// <summary>
+        /// Automatically hides the archipelago letters that are "empty".
+        /// A letter is considered empty when it contains neither a physical item, nor a code snippet to run
+        /// Examples include NPC Hearts, Seasons, Carpenter building unlocks, etc
+        /// </summary>
+        public bool HideEmptyArchipelagoLetters { get; set; } = false;
     }
 }
