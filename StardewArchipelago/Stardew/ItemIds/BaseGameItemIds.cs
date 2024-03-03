ad8656d6de22c7643c95d3ba4d3d02b4c9a13fe8
Alex Gilbert <alexgilbert@yahoo.com>
2024-03-03 00:04:35 -0500

    - Fixed FishingInjections

--- StardewArchipelago/Stardew/ItemIds/BaseGameItemIds.cs
+++ StardewArchipelago/Stardew/ItemIds/BaseGameItemIds.cs
@@ -9,11 +9,33 @@ namespace StardewArchipelago.Stardew.ItemIds
     public static class BaseGameItemIds
     {
         public const string WEEDS = "0";
+
+        public const string GOLDEN_WALNUT = "73";
+        public const string SECRET_NOTE = "79";
+        public const string SEAWEED = "152";
+        public const string GREEN_ALGAE = "153";
+        public const string WHITE_ALGAE = "157";
+
+        public const string JOJA_COLA = "167";
+        public const string TRASH = "168";
+        public const string DRIFTWOOD = "169";
+        public const string BROKEN_GLASSES = "170";
+        public const string BROKEN_CD = "171";
+        public const string SOGGY_NEWSPAPER = "172";
+
+        public const string ORNATE_NECKLACE = "191";
+
         public const string SPRING_ONION = "399";
+        public const string SUNFLOWER = "421";
+        public const string SUNFLOWER_SEEDS = "431";
         public const string ANCIENT_FRUIT = "454";
         public const string FIBER = "771";
         public const string QI_FRUIT = "889";
-        public const string SUNFLOWER = "421";
-        public const string SUNFLOWER_SEEDS = "431";
+
+        public const string PEARL = "797";
+        public const string FOSSILIZED_SPINE = "821";
+        public const string SNAKE_SKULL = "825";
+        public const string JOURNAL_SCRAP = "842";
+        public const string QI_BEAN = "890";
     }
 }
