599bc61d17c63f00ab285716efeab786ae1b8ddf
Alex Gilbert <alexgilbert@yahoo.com>
2024-03-03 00:42:39 -0500

    - Added HasReceivedItem Game State Condition

---
+++ StardewArchipelago/Stardew/Ids/GameStateConditionProvider.cs
@@ -0,0 +1,42 @@
+﻿using System.Linq;
+using System.Reflection;
+using StardewArchipelago.Locations;
+
+namespace StardewArchipelago.Stardew.Ids
+{
+    public static class GameStateConditionProvider
+    {
+        public static readonly string HAS_RECEIVED_ITEM = CreateId("HasReceivedItem");
+
+        public static string CreateHasReceivedItemCondition(string itemName, int amount = 1)
+        {
+            if (amount < 1)
+            {
+                return string.Empty;
+            }
+
+            var arguments = amount == 1 ? new[] { itemName } : new[] { itemName, amount.ToString() };
+            return CreateCondition(HAS_RECEIVED_ITEM, arguments);
+        }
+
+        public static string CreateCondition(string condition, string[] arguments)
+        {
+            return !arguments.Any() ? condition : $"{condition} {string.Join(' ', arguments)}";
+        }
+
+        private static string CreateId(string name)
+        {
+            return $"{ModEntry.Instance.ModManifest.UniqueID}.GameStateCondition.{name}";
+        }
+
+        private static string CreateId(MemberInfo t)
+        {
+            return CreateId(t.Name);
+        }
+
+        private static string CreateId<T>()
+        {
+            return CreateId(typeof(T));
+        }
+    }
+}
--- StardewArchipelago/Stardew/Ids/Vanilla/ItemIds.cs
+++ StardewArchipelago/Stardew/Ids/Vanilla/ItemIds.cs
@@ -19,17 +19,32 @@
 
         public const string ORNATE_NECKLACE = "191";
 
+        public const string TROUT_SOUP = "219";
+
         public const string SPRING_ONION = "399";
         public const string SUNFLOWER = "421";
         public const string SUNFLOWER_SEEDS = "431";
         public const string ANCIENT_FRUIT = "454";
+
+        public const string BAIT = "685";
+        public const string SPINNER = "686";
+        public const string DRESSED_SPINNER = "687";
+        public const string BARBED_HOOK = "691";
+        public const string LEAD_BOBBER = "692";
+        public const string TREASURE_HUNTER = "693";
+        public const string TRAP_BOBBER = "694";
+        public const string CORK_BOBBER = "695";
+        public const string MAGNET = "703";
+        public const string CRAB_POT = "710";
+
         public const string FIBER = "771";
-        public const string QI_FRUIT = "889";
 
         public const string PEARL = "797";
         public const string FOSSILIZED_SPINE = "821";
         public const string SNAKE_SKULL = "825";
         public const string JOURNAL_SCRAP = "842";
+        public const string QI_FRUIT = "889";
         public const string QI_BEAN = "890";
+
     }
 }
