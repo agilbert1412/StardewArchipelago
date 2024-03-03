3f9c82fb2844eebfd962fba390075f27897615fa
Alex Gilbert <alexgilbert@yahoo.com>
2024-03-02 23:57:48 -0500

    - Add BaseGameItemIds

---
+++ StardewArchipelago/Stardew/ItemIds/BaseGameItemIds.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StardewArchipelago.Stardew.ItemIds
+{
+    public static class BaseGameItemIds
+    {
+        public const string WEEDS = "0";
+        public const string SPRING_ONION = "399";
+        public const string ANCIENT_FRUIT = "454";
+        public const string FIBER = "771";
+        public const string QI_FRUIT = "889";
+        public const string SUNFLOWER = "421";
+        public const string SUNFLOWER_SEEDS = "431";
+    }
+}
