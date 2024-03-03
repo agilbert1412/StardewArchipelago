c80ace45b6af4bc3542f84cf5c1878b3a878235c
Alex Gilbert <alexgilbert@yahoo.com>
2024-03-03 00:04:35 -0500

    - Fixed FishingInjections

--- StardewArchipelago/Locations/CodeInjections/Vanilla/FishingInjections.cs
+++ StardewArchipelago/Locations/CodeInjections/Vanilla/FishingInjections.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using StardewArchipelago.Archipelago;
 using StardewArchipelago.Goals;
 using StardewArchipelago.Stardew;
+using StardewArchipelago.Stardew.ItemIds;
 using StardewModdingAPI;
 using StardewValley;
 
@@ -10,22 +11,17 @@ namespace StardewArchipelago.Locations.CodeInjections.Vanilla
 {
     public static class FishingInjections
     {
-        private const int GREEN_ALGAE = 153;
-        private const int WHITE_ALGAE = 157;
-        private const int SEAWEED = 152;
-        private const int ORNATE_NECKLACE = 191;
-        private const int GOLDEN_WALNUT = 73;
-        private const int SECRET_NOTE = 79;
-        private const int PEARL = 797;
-        private const int FOSSILIZED_SPINE = 821;
-        private const int SNAKE_SKULL = 825;
-        private const int JOURNAL_SCRAP = 842;
-        private const int QI_BEAN = 890;
-
-        private static readonly int[] _fishsanityExceptions = new[]
+        private static readonly string[] _fishedTrash = new[]
         {
-            GREEN_ALGAE, WHITE_ALGAE, SEAWEED, ORNATE_NECKLACE, GOLDEN_WALNUT, SECRET_NOTE, FOSSILIZED_SPINE, PEARL, SNAKE_SKULL, JOURNAL_SCRAP,
-            QI_BEAN,
+            BaseGameItemIds.JOJA_COLA, BaseGameItemIds.TRASH, BaseGameItemIds.DRIFTWOOD,
+            BaseGameItemIds.BROKEN_GLASSES, BaseGameItemIds.BROKEN_CD, BaseGameItemIds.SOGGY_NEWSPAPER,
+        };
+
+        private static readonly string[] _fishsanityExceptions = new[]
+        {
+            BaseGameItemIds.GREEN_ALGAE, BaseGameItemIds.WHITE_ALGAE, BaseGameItemIds.SEAWEED, BaseGameItemIds.ORNATE_NECKLACE, BaseGameItemIds.GOLDEN_WALNUT, 
+            BaseGameItemIds.SECRET_NOTE, BaseGameItemIds.FOSSILIZED_SPINE, BaseGameItemIds.PEARL, BaseGameItemIds.SNAKE_SKULL, BaseGameItemIds.JOURNAL_SCRAP,
+            BaseGameItemIds.QI_BEAN,
         };
         public const string FISHSANITY_PREFIX = "Fishsanity: ";
 
@@ -44,25 +40,26 @@ namespace StardewArchipelago.Locations.CodeInjections.Vanilla
             _itemManager = itemManager;
         }
 
-        public static void CaughtFish_Fishsanity_Postfix(Farmer __instance, int index, int size, bool from_fish_pond, int numberCaught, ref bool __result)
+        // public bool caughtFish(string itemId, int size, bool from_fish_pond = false, int numberCaught = 1)
+        public static void CaughtFish_Fishsanity_Postfix(Farmer __instance, string itemId, int size, bool from_fish_pond, int numberCaught, ref bool __result)
         {
             try
             {
-                if (from_fish_pond || (index >= 167 && index < 173) || !_itemManager.ObjectExists(index))
+                if (from_fish_pond || (IsFishedTrash(itemId)) || !_itemManager.ObjectExists(itemId))
                 {
                     return;
                 }
 
-                var fish = _itemManager.GetObjectById(index);
+                var fish = _itemManager.GetObjectById(itemId);
                 var fishName = fish.Name;
                 var apLocation = $"{FISHSANITY_PREFIX}{fishName}";
                 if (_archipelago.GetLocationId(apLocation) > -1)
                 {
                     _locationChecker.AddCheckedLocation(apLocation);
                 }
-                else if (!_fishsanityExceptions.Contains(index))
+                else if (!_fishsanityExceptions.Contains(itemId))
                 {
-                    _monitor.Log($"Unrecognized Fishsanity Location: {fishName} [{index}]", LogLevel.Error);
+                    _monitor.Log($"Unrecognized Fishsanity Location: {fishName} [{itemId}]", LogLevel.Error);
                 }
             }
             catch (Exception ex)
@@ -83,6 +80,11 @@ namespace StardewArchipelago.Locations.CodeInjections.Vanilla
                 _monitor.Log($"Failed in {nameof(CaughtFish_CheckGoalCompletion_Postfix)}:\n{ex}", LogLevel.Error);
                 return;
             }
+        }
+
+        private static bool IsFishedTrash(string itemId)
+        {
+            return _fishedTrash.Contains(itemId);
         }
     }
 }
