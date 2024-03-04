175fe1e39e441bfdb542b2004b3c45b65b3872f1
Alex Gilbert <alexgilbert@yahoo.com>
2024-03-03 23:04:29 -0500

    - Fixed Boat Tunnel Injections

--- StardewArchipelago/Stardew/StardewId.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace StardewArchipelago.Stardew
-{
-    public class StardewId
-    {
-        public const int IRIDIUM_BAR = 337;
-        public const int HARDWOOD = 709;
-        public const int BATTERY_PACK = 787;
-    }
-}
