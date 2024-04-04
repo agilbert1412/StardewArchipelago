97bd927f0dda3376bf6689110e1ab7e4ccae760c
Alex Gilbert <alexgilbert@yahoo.com>
2024-04-03 21:11:05 -0400

    - Pre alpha version number (or something)

--- StardewArchipelago/Stardew/StardewFlavoredObject.cs
+++ StardewArchipelago/Stardew/StardewFlavoredObject.cs
@@ -1,40 +1,40 @@
-﻿using StardewArchipelago.Archipelago;
-using StardewArchipelago.Constants.Vanilla;
-using StardewArchipelago.Items.Mail;
-using StardewValley;
+﻿//using StardewArchipelago.Archipelago;
+//using StardewArchipelago.Constants.Vanilla;
+//using StardewArchipelago.Items.Mail;
+//using StardewValley;
 
-namespace StardewArchipelago.Stardew
-{
-    public class StardewFlavoredObject : StardewObject
-    {
-        public StardewObject FlavorItem { get; private set; }
-        public override string Name { get; protected set; }
+//namespace StardewArchipelago.Stardew
+//{
+//    public class StardewFlavoredObject : StardewObject
+//    {
+//        public StardewObject FlavorItem { get; private set; }
+//        public override string Name { get; protected set; }
 
-        public StardewFlavoredObject(StardewObject flavorItem, string id, string name, int sellPrice, int edibility, string type, int category, string displayName, string description)
-        : base(id, name, sellPrice, edibility, type, category, displayName, description)
-        {
-            FlavorItem = flavorItem;
-        }
+//        public StardewFlavoredObject(StardewObject flavorItem, string id, string name, int sellPrice, int edibility, string type, int category, string displayName, string description)
+//        : base(id, name, sellPrice, edibility, type, category, displayName, description)
+//        {
+//            FlavorItem = flavorItem;
+//        }
 
-        public override Item PrepareForGivingToFarmer(int amount = 1)
-        {
-            return new Object(Id, amount);
-        }
+//        public override Item PrepareForGivingToFarmer(int amount = 1)
+//        {
+//            return new Object(Id, amount);
+//        }
 
-        public override void GiveToFarmer(Farmer farmer, int amount = 1)
-        {
-            var item = PrepareForGivingToFarmer(amount);
-            farmer.addItemByMenuIfNecessary(item);
-        }
+//        public override void GiveToFarmer(Farmer farmer, int amount = 1)
+//        {
+//            var item = PrepareForGivingToFarmer(amount);
+//            farmer.addItemByMenuIfNecessary(item);
+//        }
 
-        public override LetterAttachment GetAsLetter(ReceivedItem receivedItem, int amount = 1)
-        {
-            return new LetterItemAttachment(receivedItem, this, amount);
-        }
+//        public override LetterAttachment GetAsLetter(ReceivedItem receivedItem, int amount = 1)
+//        {
+//            return new LetterItemAttachment(receivedItem, this, amount);
+//        }
 
-        public override string GetQualifiedId()
-        {
-            return $"{QualifiedItemIds.OBJECT_QUALIFIER}{Id}";
-        }
-    }
-}
+//        public override string GetQualifiedId()
+//        {
+//            return $"{QualifiedItemIds.OBJECT_QUALIFIER}{Id}";
+//        }
+//    }
+//}
