10a6db9099b4f018448dcfd21fef8eff8213543e
Alex Gilbert <alexgilbert@yahoo.com>
2024-04-09 23:26:27 -0400

    - Finished implementing the traveling merchant

---
+++ StardewArchipelago/Constants/TriggerActionProvider.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using System.Reflection;
+
+namespace StardewArchipelago.Constants
+{
+    public static class TriggerActionProvider
+    {
+        public static readonly string TRAVELING_MERCHANT_PURCHASE = CreateId("TravelingMerchantPurchase");
+        
+
+        private static string CreateId(string name)
+        {
+            return $"{ModEntry.Instance.ModManifest.UniqueID}.TriggerAction.{name}";
+        }
+
+        private static string CreateId(MemberInfo t)
+        {
+            return CreateId(t.Name);
+        }
+
+        private static string CreateId<T>()
+        {
+            return CreateId(typeof(T));
+        }
+    }
+}
