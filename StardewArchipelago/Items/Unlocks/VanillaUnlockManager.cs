f49db22b9ad6367430768d308ceed068475a20db
Alex Gilbert <alexgilbert@yahoo.com>
2024-04-25 21:17:32 -0400

    - Added support for receiving masteries
    # Conflicts:
    #	StardewArchipelago/Items/Unlocks/VanillaUnlockManager.cs

--- StardewArchipelago/Items/Unlocks/VanillaUnlockManager.cs
+++ StardewArchipelago/Items/Unlocks/VanillaUnlockManager.cs
@@ -6,6 +6,7 @@ using StardewArchipelago.Extensions;
 using StardewArchipelago.Items.Mail;
 using StardewArchipelago.Locations.CodeInjections.Vanilla;
 using StardewValley;
+using StardewValley.Constants;
 
 namespace StardewArchipelago.Items.Unlocks
 {
@@ -92,6 +93,16 @@ namespace StardewArchipelago.Items.Unlocks
             _unlockables.Add($"{Skill.Foraging} Level", SendProgressiveForagingLevel);
             _unlockables.Add($"{Skill.Mining} Level", SendProgressiveMiningLevel);
             _unlockables.Add($"{Skill.Combat} Level", SendProgressiveCombatLevel);
+            RegisterPlayerMasteries();
+        }
+
+        private void RegisterPlayerMasteries()
+        {
+            _unlockables.Add($"{Skill.Farming} Mastery", SendFarmingMastery);
+            _unlockables.Add($"{Skill.Fishing} Mastery", SendFishingMastery);
+            _unlockables.Add($"{Skill.Foraging} Mastery", SendForagingMastery);
+            _unlockables.Add($"{Skill.Mining} Mastery", SendMiningMastery);
+            _unlockables.Add($"{Skill.Combat} Mastery", SendCombatMastery);
         }
 
         private void RegisterProgressiveTools()
@@ -482,6 +493,86 @@ namespace StardewArchipelago.Items.Unlocks
                 farmer.newLevels.Add(new Point((int)skill, newLevel));
             }
             return new LetterInformationAttachment(receivedItem);
+        }
+
+        private LetterAttachment SendFarmingMastery(ReceivedItem receivedItem)
+        {
+            const Skill skill = Skill.Farming;
+            foreach (var farmer in Game1.getAllFarmers())
+            {
+                AddCraftingRecipes(farmer, new[] { "Statue Of Blessings" });
+                GiveItemsToFarmer(farmer, "(W)66");
+                farmer.stats.Increment(StatKeys.Mastery((int)skill), 1);
+            }
+            return new LetterInformationAttachment(receivedItem);
+        }
+
+        private LetterAttachment SendForagingMastery(ReceivedItem receivedItem)
+        {
+            const Skill skill = Skill.Foraging;
+            foreach (var farmer in Game1.getAllFarmers())
+            {
+                AddCraftingRecipes(farmer, new[] { "Mystic Tree Seed", "Treasure Totem" });
+                farmer.stats.Increment(StatKeys.Mastery((int)skill), 1);
+            }
+            return new LetterInformationAttachment(receivedItem);
+        }
+
+        private LetterAttachment SendFishingMastery(ReceivedItem receivedItem)
+        {
+            const Skill skill = Skill.Fishing;
+            foreach (var farmer in Game1.getAllFarmers())
+            {
+                AddCraftingRecipes(farmer, new[] { "Challenge Bait" });
+                GiveItemsToFarmer(farmer, "(T)AdvancedIridiumRod");
+                farmer.stats.Increment(StatKeys.Mastery((int)skill), 1);
+            }
+            return new LetterInformationAttachment(receivedItem);
+        }
+
+        private LetterAttachment SendMiningMastery(ReceivedItem receivedItem)
+        {
+            const Skill skill = Skill.Mining;
+            foreach (var farmer in Game1.getAllFarmers())
+            {
+                AddCraftingRecipes(farmer, new[] { "Statue Of The Dwarf King", "Heavy Furnace" });
+                farmer.stats.Increment(StatKeys.Mastery((int)skill), 1);
+            }
+            return new LetterInformationAttachment(receivedItem);
+        }
+
+        private LetterAttachment SendCombatMastery(ReceivedItem receivedItem)
+        {
+            const Skill skill = Skill.Combat;
+            foreach (var farmer in Game1.getAllFarmers())
+            {
+                AddCraftingRecipes(farmer, new[] { "Anvil", "Mini-Forge" });
+                Game1.player.stats.Set("trinketSlots", 1);
+                farmer.stats.Increment(StatKeys.Mastery((int)skill), 1);
+            }
+            return new LetterInformationAttachment(receivedItem);
+        }
+
+        private void AddCraftingRecipes(Farmer farmer, IEnumerable<string> recipes)
+        {
+            foreach (var recipe in recipes)
+            {
+                farmer.craftingRecipes.Add(recipe, 0);
+            }
+        }
+
+        private void GiveItemsToFarmer(Farmer farmer, string itemId)
+        {
+            if (_archipelago.SlotData.ToolProgression.HasFlag(ToolProgression.Progressive))
+            {
+                return;
+            }
+
+            var item = ItemRegistry.Create(itemId);
+            if (!farmer.addItemToInventoryBool(item))
+            {
+                Game1.createItemDebris(item, Game1.player.getStandingPosition(), 2);
+            }
         }
 
         public void GiveExperienceToNextLevel(Farmer farmer, Skill skill)
