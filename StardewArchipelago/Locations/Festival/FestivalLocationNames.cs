baada239b82e712ebce078d6a0af744263c53ed3
Alex Gilbert <alexgilbert@yahoo.com>
2024-05-04 21:51:52 -0400

    - Add some festival location names

--- StardewArchipelago/Locations/Festival/FestivalLocationNames.cs
+++ StardewArchipelago/Locations/Festival/FestivalLocationNames.cs
@@ -17,10 +17,55 @@ namespace StardewArchipelago.Locations.Festival
         public const string NIGHT_MARKET_15 = "Night Market 15";
         public const string NIGHT_MARKET_16 = "Night Market 16";
         public const string NIGHT_MARKET_17 = "Night Market 17";
+        public const string DESERT_FESTIVAL_ALL = "Desert Festival All";
+        public const string DESERT_FESTIVAL_15 = "Desert Festival 15";
+        public const string DESERT_FESTIVAL_16 = "Desert Festival 16";
+        public const string DESERT_FESTIVAL_17 = "Desert Festival 17";
+        public const string TROUT_DERBY_ALL = "Trout Derby All";
+        public const string TROUT_DERBY_20 = "Trout Derby 20";
+        public const string TROUT_DERBY_21 = "Trout Derby 21";
+        public const string SQUIDFEST_ALL = "SquidFest All";
+        public const string SQUIDFEST_12 = "SquidFest 12";
+        public const string SQUIDFEST_13 = "SquidFest 13";
 
 
         public const string EGG_HUNT = "Egg Hunt Victory";
         public const string STRAWBERRY_SEEDS = "Egg Festival: Strawberry Seeds";
+
+        public const string CALICO_RACE = "Calico Race";
+        public const string MUMMY_MASK = "Mummy Mask";
+        public const string CALICO_STATUE = "Calico Statue";
+        public const string EMILYS_OUTFIT_SERVICES = "Emily's Outfit Services";
+        public const string EARTHY_MOUSSE = "Earthy Mousse";
+        public const string SWEET_BEAN_CAKE = "Sweet Bean Cake";
+        public const string SKULL_CAVERN_CASSEROLE = "Skull Cavern Casserole";
+        public const string SPICY_TACOS = "Spicy Tacos";
+        public const string MOUNTAIN_CHILI = "Mountain Chili";
+        public const string CRYSTAL_CAKE = "Crystal Cake";
+        public const string CAVE_KEBAB = "Cave Kebab";
+        public const string HOT_LOG = "Hot Log";
+        public const string SOUR_SALAD = "Sour Salad";
+        public const string SUPERFOOD_CAKE = "Superfood Cake";
+        public const string WARRIOR_SMOOTHIE = "Warrior Smoothie";
+        public const string RUMPLED_FRUIT_SKIN = "Rumpled Fruit Skin";
+        public const string CALICO_PIZZA = "Calico Pizza";
+        public const string STUFFED_MUSHROOMS = "Stuffed Mushrooms";
+        public const string ELF_QUESADILLA = "Elf Quesadilla";
+        public const string NACHOS_OF_THE_DESERT = "Nachos Of The Desert";
+        public const string CLOPPINO = "Cloppino";
+        public const string RAINFOREST_SHRIMP = "Rainforest Shrimp";
+        public const string SHRIMP_DONUT = "Shrimp Donut";
+        public const string SMELL_OF_THE_SEA = "Smell Of The Sea";
+        public const string DESERT_GUMBO = "Desert Gumbo";
+        public const string FREE_CACTUS = "Free Cactus";
+        public const string MONSTER_HUNT = "Monster Hunt";
+        public const string DEEP_DIVE = "Deep Dive";
+        public const string TREASURE_HUNT = "Treasure Hunt";
+        public const string TOUCH_A_CALICO_STATUE = "Touch A Calico Statue";
+        public const string REAL_CALICO_EGG_HUNTER = "Real Calico Egg Hunter";
+        public const string WILLYS_CHALLENGE = "Willy's Challenge";
+        public const string DESERT_SCHOLAR = "Desert Scholar";
+
         public const string DANCE_WITH_SOMEONE = "Dance with someone";
         public const string RARECROW_5 = "Rarecrow #5 (Woman)";
         public const string TUB_O_FLOWERS_RECIPE = "Tub o' Flowers Recipe";
