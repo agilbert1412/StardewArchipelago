43d06092e40f22f729c0014d4666fdd7b580fb9f
Alex Gilbert <alexgilbert@yahoo.com>
2024-05-11 21:02:55 -0400

    - Fixed book reading animation

--- StardewArchipelago/Locations/CodeInjections/Vanilla/BookInjections.cs
+++ StardewArchipelago/Locations/CodeInjections/Vanilla/BookInjections.cs
@@ -293,7 +293,7 @@ namespace StardewArchipelago.Locations.CodeInjections.Vanilla
             Game1.Multiplayer.broadcastSprites(location, new TemporaryAnimatedSprite("LooseSprites\\Book_Animation", new Microsoft.Xna.Framework.Rectangle(0, 0, 20, 20), 10f, 45, 1, Game1.player.getStandingPosition() + new Vector2(-48f, -156f), false, false, Game1.player.getDrawLayer() + 1f / 1000f, 0.0f, Color.White, 4f, 0.0f, 0.0f, 0.0f)
             {
                 holdLastFrame = true,
-                id = 1987654
+                id = 1987654,
             });
             var colorFromTags = ItemContextTagManager.GetColorFromTags(book);
             if (colorFromTags.HasValue)
@@ -301,7 +301,7 @@ namespace StardewArchipelago.Locations.CodeInjections.Vanilla
                 Game1.Multiplayer.broadcastSprites(location, new TemporaryAnimatedSprite("LooseSprites\\Book_Animation", new Microsoft.Xna.Framework.Rectangle(0, 20, 20, 20), 10f, 45, 1, Game1.player.getStandingPosition() + new Vector2(-48f, -156f), false, false, Game1.player.getDrawLayer() + 0.0012f, 0.0f, colorFromTags.Value, 4f, 0.0f, 0.0f, 0.0f)
                 {
                     holdLastFrame = true,
-                    id = 1987654
+                    id = 1987654,
                 });
             }
         }
