d2f145eea0f2d6f9ca5089c245c29068273ae6b8
Witchybun <embenham05@gmail.com>
2024-06-05 05:16:28 -0500

    Remove methods from ids

--- StardewArchipelago/Constants/Modded/ModItemIds.cs
+++ StardewArchipelago/Constants/Modded/ModItemIds.cs
@@ -10,25 +10,5 @@ namespace StardewArchipelago.Constants.Modded
         public static readonly string TEMPERED_DAGGER = "FlashShifter.StardewValleyExpandedCP_Tempered_Galaxy_Dagger";
         public static readonly string TEMPERED_SWORD = "FlashShifter.StardewValleyExpandedCP_Tempered_Galaxy_Sword";
         public static readonly string TEMPERED_HAMMER = "FlashShifter.StardewValleyExpandedCP_Tempered_Galaxy_Hammer";
-
-        private static string QualifiedObjectId(string objectId)
-        {
-            return $"{QualifiedItemIds.OBJECT_QUALIFIER}{objectId}";
-        }
-
-        private static string QualifiedWeaponId(string weaponId)
-        {
-            return $"{QualifiedItemIds.WEAPON_QUALIFIER}{weaponId}";
-        }
-
-        private static bool IsType(string itemId, string qualifier)
-        {
-            if (string.IsNullOrWhiteSpace(itemId))
-            {
-                return false;
-            }
-
-            return itemId.StartsWith(qualifier, StringComparison.InvariantCultureIgnoreCase);
-        }
     }
 }
