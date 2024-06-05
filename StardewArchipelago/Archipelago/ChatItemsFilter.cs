28cb129f999054df91315921761dc75418b375aa
Alex Gilbert <alexgilbert@yahoo.com>
2024-06-05 10:59:42 -0400

    - Add new config options to fine tune the chatbox behavior
    # Conflicts:
    #	StardewArchipelago/Archipelago/ArchipelagoClient.cs
    #	StardewArchipelago/Integrations/GenericModConfigMenu/ConfigMenu.cs
    #	StardewArchipelago/ModConfig.cs

---
+++ StardewArchipelago/Archipelago/ChatItemsFilter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StardewArchipelago.Archipelago
+{
+    public enum ChatItemsFilter
+    {
+        None,
+        RelatedToMe,
+        All
+    }
+}
