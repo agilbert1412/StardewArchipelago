86a04dc79fc666a28ea1388ca07cd268d008fff9
Alex Gilbert <alexgilbert@yahoo.com>
2024-06-09 14:38:04 -0400

    - Attempt at fixing a queen of sauce error with UIIS2

--- StardewArchipelago/Locations/CodeInjections/Vanilla/QueenOfSauceInjections.cs
+++ StardewArchipelago/Locations/CodeInjections/Vanilla/QueenOfSauceInjections.cs
@@ -51,20 +51,8 @@ namespace StardewArchipelago.Locations.CodeInjections.Vanilla
                     return false; // don't run original logic
                 }
 
-                // private TemporaryAnimatedSprite screen;
-                var screenField = _helper.Reflection.GetField<TemporaryAnimatedSprite>(__instance, "screen");
-
-                // private int currentChannel;
-                var currentChannelField = _helper.Reflection.GetField<int>(__instance, "currentChannel");
-
-                if (screenField.GetValue() == null || currentChannelField.GetValue() != 5)
-                {
-                    return false; // don't run original logic
-                }
-
                 var cookingChannelData = DataLoader.Tv_CookingChannel(Game1.temporaryContent);
                 var recipeWeek = PickRecipeWeekToTeach(cookingChannelData);
-
 
                 var recipeInfo = cookingChannelData[recipeWeek.ToString()].Split('/');
                 var recipeName = recipeInfo[0];
