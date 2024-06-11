079436681efda593d9cc4df2f0902ae702b1066e
Alex Gilbert <alexgilbert@yahoo.com>
2024-06-10 23:43:02 -0400

    - Added some community names

--- StardewArchipelago/Constants/Community.cs
+++ StardewArchipelago/Constants/Community.cs
@@ -4,14 +4,14 @@
     {
         public static readonly string[] AllNames = new[]
         {
-            "Albrekka", "Alchav", "Angela", "axe_y", "beauxq", "Berserker", "Black Sliver", "blastron",
+            "Albrekka", "Alchav", "Angela", "ArgonMatrix", "axe_y", "beauxq", "Berserker", "Black Sliver", "blastron",
             "BootsinSoots", "BruceWinless", "CaitSith2", "Captain Canadia", "Celeste", "Damocles", "dewin",
             "el_", "emmalution", "Espeon", "eudaimonistic", "Exempt-Medic", "Farrak Kilhn", "Figment",
             "Fly Sniper", "Frazzleduck", "Heinermann", "Ishigh", "JaredWeakStrike", "Jarno", "jat2980",
             "jennnifer", "Jouramie", "Justice", "Kaito Kid", "Kankuro", "KelioMZX", "Kimou00", "KittyAshy",
-            "Kono Tyran", "Lady Redacted", "Liarose", "lordlou", "Madolinn", "Magnemania", "Marech", "Margot",
+            "Kono Tyran", "Lady Redacted", "Liarose", "lordlou", "MaeDawn", "Madolinn", "Magnemania", "Marech", "Margot",
             "Mati", "Mav", "MeleneV", "mewlif", "Mistik", "Nada", "Noca", "N00byKing" /*, "NinoKito"*/,
-            "Phar", "PoryGone", "ProfBytes", "RaspberrySpaceJam", "Relissa", "Rosalie", "Salzkorn", "SamanthaCoffin",
+            "Phar", "PoryGone", "ProfBytes", "RaspberrySpaceJam", "Relissa", "Rosalie", "Salmence", "Salzkorn", "SamanthaCoffin",
             "Scipio", "Smorgasbord", "Sneaki", "Snow", "Sraw", "SunnyBat", "TheCondor", "TheHaboo",
             "TheRedWolfe", "toaster", "Trev", "Violet", "WhySoThirstyy", "Yellow_Meep", "zig", "Ziktofel",
         };
