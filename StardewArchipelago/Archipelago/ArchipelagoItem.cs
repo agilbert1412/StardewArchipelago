1546c8977d152cb38a1b5ce68f1d52ef03acd79d
Alex Gilbert <alexgilbert@yahoo.com>
2024-06-13 20:03:01 -0400

    - Made the item classification enum into flags that includes skip balancing

--- StardewArchipelago/Archipelago/ArchipelagoItem.cs
+++ StardewArchipelago/Archipelago/ArchipelagoItem.cs
@@ -39,11 +39,16 @@ namespace StardewArchipelago.Archipelago
         }
     }
 
+    [Flags]
     public enum ItemClassification
     {
-        Progression,
-        Useful,
-        Filler,
-        Trap,
+        Filler = 0b0000,
+        Progression = 0b0001,
+        Useful = 0b0010,
+        Trap = 0b0100,
+        SkipBalancing = 0b1000,
+        ProgressionSkipBalancing = Progression | SkipBalancing,
+        skip_balancing = SkipBalancing,
+        progression_skip_balancing = ProgressionSkipBalancing,
     }
 }
