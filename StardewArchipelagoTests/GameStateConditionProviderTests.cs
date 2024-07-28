19733fa701362d82c1529aba7dd473cb9249d4a2
Alex Gilbert <alexgilbert@yahoo.com>
2024-07-28 19:01:36 -0400

    - Fixed typos

--- StardewArchipelagoTests/GameStateConditionProviderTests.cs
+++ StardewArchipelagoTests/GameStateConditionProviderTests.cs
@@ -17,9 +17,9 @@ namespace StardewArchipelagoTests
         [TestCase("Deluxe Coop", "!BUILDINGS_CONSTRUCTED ALL \"Deluxe Coop\" 0 0")]
         [TestCase("Big Coop", "ANY \"!BUILDINGS_CONSTRUCTED ALL \\\"Big Coop\\\" 0 0\" \"!BUILDINGS_CONSTRUCTED ALL \\\"Deluxe Coop\\\" 0 0\"")]
         [TestCase("Coop", "ANY \"!BUILDINGS_CONSTRUCTED ALL Coop 0 0\" \"!BUILDINGS_CONSTRUCTED ALL \\\"Big Coop\\\" 0 0\" \"!BUILDINGS_CONSTRUCTED ALL \\\"Deluxe Coop\\\" 0 0\"")]
-        [TestCase("Big Shed", "ANY \"!BUILDINGS_CONSTRUCTED ALL \\\"Big Shed\\\" 0 0\"")]
-        [TestCase("Shed", "ANY \"!BUILDINGS_CONSTRUCTED ALL Shed 0 0\" \"!BUILDINGS_CONSTRUCTED ALL \\\"Big Shed\\\" 0 0\"")]
-        [TestCase("Well", "!BUILDINGS_CONSTRUCTED ALL Well 0 ")]
+        [TestCase("Big Shed", "ANY \"!BUILDINGS_CONSTRUCTED ALL \\\"Big Shed\\\" 0 0\" \"!BUILDINGS_CONSTRUCTED ALL \\\"Deluxe Shed\\\" 0 0\"")]
+        [TestCase("Shed", "ANY \"!BUILDINGS_CONSTRUCTED ALL Shed 0 0\" \"!BUILDINGS_CONSTRUCTED ALL \\\"Big Shed\\\" 0 0\" \"!BUILDINGS_CONSTRUCTED ALL \\\"Deluxe Shed\\\" 0 0\"")]
+        [TestCase("Well", "!BUILDINGS_CONSTRUCTED ALL Well 0 0")]
         [TestCase("Slime Hutch", "!BUILDINGS_CONSTRUCTED ALL \"Slime Hutch\" 0 0")]
         [TestCase("Silo", "!BUILDINGS_CONSTRUCTED ALL Silo 0 0")]
         public void HasBuildingOrHigher(string buildingName, string expectedCondition)
