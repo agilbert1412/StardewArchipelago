3b1f007b36fa528e8f4da263246f76f0d272c4d6
Alex Gilbert <alexgilbert@yahoo.com>
2024-11-08 16:16:39 -0500

    WIP Jojapocalypse

---
+++ StardewArchipelago/Locations/JojapocalypseManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StardewArchipelago.Locations
+{
+    public class JojapocalypseManager
+    {
+        public HashSet<string> JojaCheckedLocations { get; set; }
+
+        public JojapocalypseManager()
+        {
+
+        }
+
+    }
+}
