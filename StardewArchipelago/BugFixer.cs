1759bf55de555d981a2ec6fc9e6a20a18104ed45
Alex Gilbert <alexgilbert@yahoo.com>
2024-12-31 15:58:46 -0500

    - Auto send the broken Water Shifter check

--- StardewArchipelago/BugFixer.cs
+++ StardewArchipelago/BugFixer.cs
@@ -1,16 +1,27 @@
-﻿using StardewArchipelago.Locations.CodeInjections.Vanilla;
+﻿using Archipelago.MultiClient.Net.Helpers;
+using KaitoKid.ArchipelagoUtilities.Net;
+using KaitoKid.ArchipelagoUtilities.Net.Interfaces;
+using StardewArchipelago.Locations.CodeInjections.Vanilla;
 using StardewValley;
 
 namespace StardewArchipelago
 {
     public class BugFixer
     {
-        public BugFixer() { }
+        private readonly ILogger _logger;
+        private readonly LocationChecker _locationChecker;
+
+        public BugFixer(ILogger logger, LocationChecker locationChecker)
+        {
+            _logger = logger;
+            _locationChecker = locationChecker;
+        }
 
 
         public void FixKnownBugs()
         {
             FixFreeBuildingsWithFreeInTheId();
+            FixShipsanityWaterShifter();
         }
 
         private static void FixFreeBuildingsWithFreeInTheId()
@@ -23,6 +34,11 @@ namespace StardewArchipelago
                     building.buildingType.Set(CarpenterInjections.RemoveFreePrefix(building.buildingType.Get()));
                 }
             }
+        }
+
+        private void FixShipsanityWaterShifter()
+        {
+            _locationChecker.AddCheckedLocation("Shipsanity: Water Shifter");
         }
     }
 }
--- StardewArchipelago/ModEntry.cs
+++ StardewArchipelago/ModEntry.cs
@@ -485,7 +485,7 @@ namespace StardewArchipelago
             TheaterInjections.UpdateScheduleForEveryone();
             Helper.GameContent.InvalidateCache("Data/Shops"); // This should be reworked someday
 
-            var bugFixer = new BugFixer();
+            var bugFixer = new BugFixer(_logger, _locationChecker);
             bugFixer.FixKnownBugs();
 
             _hintHelper.GiveHintTip(_archipelago.GetSession());
