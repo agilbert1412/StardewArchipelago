31b36302d0748bff262ebbbd665d32c7714fdf51
Alex Gilbert <alexgilbert@yahoo.com>
2025-01-03 11:50:12 -0500

    - WIP secrets

---
+++ StardewArchipelago/Locations/Secrets/PurpleShortsInjections.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Numerics;
+using KaitoKid.ArchipelagoUtilities.Net;
+using KaitoKid.ArchipelagoUtilities.Net.Client;
+using KaitoKid.ArchipelagoUtilities.Net.Constants;
+using KaitoKid.ArchipelagoUtilities.Net.Interfaces;
+using StardewArchipelago.Archipelago;
+using StardewArchipelago.Locations.Festival;
+using StardewModdingAPI;
+using StardewValley;
+
+namespace StardewArchipelago.Locations.Secrets
+{
+    public class PurpleShortsInjections
+    {
+        private static ILogger _logger;
+        private static IModHelper _modHelper;
+        private static ArchipelagoClient _archipelago;
+        private static LocationChecker _locationChecker;
+
+        public static void Initialize(ILogger logger, IModHelper modHelper, ArchipelagoClient archipelago, LocationChecker locationChecker)
+        {
+            _logger = logger;
+            _modHelper = modHelper;
+            _archipelago = archipelago;
+            _locationChecker = locationChecker;
+        }
+
+        // public static void SwitchEvent(Event @event, string[] args, EventContext context)
+        public static void SwitchEvent_PurpleShortsInSoup_Postfix(Event @event, string[] args, EventContext context)
+        {
+            try
+            {
+                var switchEventKey = args[1];
+                var governorReactionKey = "governorReaction";
+                if (!@event.isSpecificFestival("summer11") || !switchEventKey.StartsWith(governorReactionKey))
+                {
+                    return;
+                }
+
+                var soupScore = int.Parse(switchEventKey[governorReactionKey.Length..]);
+
+                const int SHORTS_REACTION = 6;
+                if (soupScore == SHORTS_REACTION)
+                {
+                    _locationChecker.AddCheckedLocation(SecretsLocationNames.POISON_THE_GOVERNOR);
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Failed in {nameof(SwitchEvent_PurpleShortsInSoup_Postfix)}:\n{ex}");
+                return;
+            }
+        }
+
+        // public void interpretGrangeResults()
+        public static void InterpretGrangeResults_Bribe_Postfix(Event __instance)
+        {
+            try
+            {
+                if (__instance.grangeScore == -666)
+                {
+                    _locationChecker.AddCheckedLocation(SecretsLocationNames.GRANGE_DISPLAY_BRIBE);
+                }
+
+                return;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Failed in {nameof(InterpretGrangeResults_Bribe_Postfix)}:\n{ex}");
+                return;
+            }
+        }
+
+        // public static void drawDialogue(NPC speaker)
+        public static void DrawDialogue_ShortsResponses_Prefix(NPC speaker)
+        {
+            try
+            {
+                if (speaker == null || speaker.Name != "Marnie" || speaker.CurrentDialogue.Count == 0)
+                {
+                    return;
+                }
+
+                var dialogue = speaker.CurrentDialogue.Peek();
+                if (dialogue == null)
+                {
+                    return;
+                }
+
+                SendLocationIfCorrectDialogue(speaker, dialogue, "Fair_Judged_PlayerLost_PurpleShorts", SecretsLocationNames.PURPLE_LETTUCE);
+                SendLocationIfCorrectDialogue(speaker, dialogue, "reject_789", SecretsLocationNames.CONFRONT_MARNIE);
+
+                return;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Failed in {nameof(DrawDialogue_ShortsResponses_Prefix)}:\n{ex}");
+                return;
+            }
+        }
+
+        private static void SendLocationIfCorrectDialogue(NPC speaker, Dialogue dialogue, string desiredDialogueKey, string location)
+        {
+
+            var desiredDialogue = speaker.TryGetDialogue(desiredDialogueKey);
+            if (desiredDialogue == null || dialogue.dialogues.Count != desiredDialogue.dialogues.Count)
+            {
+                return;
+            }
+
+            for (var i = 0; i < dialogue.dialogues.Count; i++)
+            {
+                if (dialogue.dialogues[i].Text != desiredDialogue.dialogues[i].Text)
+                {
+                    return;
+                }
+            }
+
+            _locationChecker.AddCheckedLocation(location);
+        }
+    }
+}
---
+++ StardewArchipelago/Locations/Secrets/SecretsLocationNames.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using StardewValley;
+
+namespace StardewArchipelago.Locations.Secrets
+{
+    public class SecretsLocationNames
+    {
+        public const string POISON_THE_GOVERNOR = "Poison The Governor";
+        public const string GRANGE_DISPLAY_BRIBE = "Grange Display Bribe";
+        public const string PURPLE_LETTUCE = "Purple Lettuce";
+        public const string MAKE_MARNIE_LAUGH = "Make Marnie Laugh";
+        public const string JUMPSCARE_LEWIS = "Jumpscare Lewis";
+        public const string CONFRONT_MARNIE = "Confront Marnie";
+        public const string LUCKY_PURPLE_BOBBER = "Lucky Purple Bobber";
+        public const string FREE_THE_FORSAKEN_SOULS = "Free The Forsaken Souls";
+        public const string SOMETHING_FOR_SANTA = "Something For Santa";
+        public const string THANK_THE_DEVS = "Thank the Devs";
+        public const string ANNOY_THE_MOON_MAN = "Annoy the Moon Man";
+        public const string ACKNOWLEDGE_THE_LONELY_STONE = "Acknowledge the Lonely Stone";
+        public const string TAKE_IN_THE_NATURE = "Take In The Nature";
+        public const string HMTGF = "??HMTGF??";
+        public const string PINKY_LEMON = "??Pinky Lemon??";
+        public const string FOROGUEMON = "??Foroguemon??";
+        public const string GALAXIES_WILL_HEED_YOUR_CRY = "Galaxies Will Heed Your Cry";
+        public const string JUNIMO_PLUSH = "Junimo Plush";
+        public const string STRANGE_SIGHTING = "Strange Sighting";
+        public const string BOAT = "'Boat'";
+        public const string DECORATIVE_TRASH_CAN = "Decorative Trash Can";
+        public const string FOLIAGE_PRINT = "Foliage Print";
+        public const string FROG_HAT = "Frog Hat";
+        public const string GOURMAND_STATUE = "Gourmand Statue";
+        public const string IRIDIUM_KROBUS = "Iridium Krobus";
+        public const string LIFESAVER = "Lifesaver";
+        public const string PHYSICS_101 = "'Physics 101'";
+        public const string PYRAMID_DECAL = "Pyramid Decal";
+        public const string SQUIRREL_FIGURINE = "Squirrel Figurine";
+        public const string VISTA = "'Vista'";
+        public const string WALL_BASKET = "Wall Basket";
+        public const string SUMMON_BONE_SERPENT = "Summon Bone Serpent";
+        public const string MEOWMERE = "Meowmere";
+        public const string A_FAMILIAR_TUNE = "A Familiar Tune";
+        public const string MERPERSON_SIGHTING = "Merperson Sighting";
+        public const string BIGFOOT = "...Bigfoot?";
+        public const string FLUBBER_EXPERIMENT = "Flubber Experiment";
+        public const string SEEMS_FISHY = "Seems Fishy";
+        public const string ME_ME_ME = "'Me me me me me me me me me me me me me me me me'";
+        public const string NICE_TRY = "Nice Try";
+        public const string ENJOY_YOUR_NEW_LIFE_HERE = "Enjoy your new life here";
+        public const string WHAT_YOU_EXPECT = "\"What'd you expect?\"";
+        public const string SECRET_IRIDIUM_STACKMASTER_TROPHY = "Secret Iridium Stackmaster Trophy";
+    }
+}
