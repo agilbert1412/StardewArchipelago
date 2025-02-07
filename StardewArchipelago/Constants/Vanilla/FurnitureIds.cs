4fde8f0c45bf6e4d6d72ab4d5728f391458cbfce
Alex Gilbert <alexgilbert@yahoo.com>
2025-02-06 21:03:36 -0500

    - Add Junimo Plush Secret

---
+++ StardewArchipelago/Constants/Vanilla/FurnitureIds.cs
@@ -0,0 +1,11 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace StardewArchipelago.Stardew.Ids.Vanilla
+{
+    public static class FurnitureIds
+    {
+        public const string JUNIMO_PLUSH = "1733";
+        public const string WALL_BASKET = "2425";
+        public const string WALL_CACTUS = "2655";
+    }
+}
