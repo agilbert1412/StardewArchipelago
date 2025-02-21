59a13a6851aaabfea2a3fb4105949d8c73903756
JCBoorgo <jcbourgault2008@yahoo.ca>
2025-02-21 12:15:08 -0500

    Upgrade item tooltips
    Lays a framework to add non-trivial locations to the list an item has in its tooltip that says where it's needed. Should be easily expandable beyond only quests, but this proves the concept.

--- StardewArchipelago/Constants/Locations/NameMatching.cs
+++ StardewArchipelago/Constants/Locations/NameMatching.cs
@@ -32,5 +32,37 @@ namespace StardewArchipelago.Constants.Locations
             { "Trash", new[] { "Trash Can Upgrade" } },
             { "Turnip", new[] { "Rarecrow #1 (Turnip Head)" } },
         };
+
+        // TODO expand that list, it only has quests currently
+        public static readonly Dictionary<string, string[]> ItemNeededForLocations = new()
+        {
+            { "Albacore", new[] { "Fish Stew" } },
+            { "Amaranth", new[] { "Cow's Delight" } },
+            { "Amethyst", new[] { "Clint's Attempt" } },
+            { "Apricot", new[] { "Fresh Fruit" } },
+            { "Battery Pack", new[] { "Pam Needs Juice" } },
+            { "Beet", new[] { "The Mysterious Qi" } },
+            { "Cauliflower", new[] { "Jodi's Request" } },
+            { "Cave Carrot", new[] { "Marnie's Request" } },
+            { "Coconut", new[] { "Exotic Spirits" } },
+            { "Hardwood", new[] { "Robin's Request", "The Giant Stump" } },
+            { "Hot Pepper", new[] { "Knee Therapy" } },
+            { "Iridium Bar", new[] { "Staff of Power" } },
+            { "Iron Bar", new[] { "A Favor For Clint" } },
+            { "Largemouth Bass", new[] { "Fish Casserole" } },
+            { "Leek", new[] { "Granny's Gift" } },
+            { "Maple Syrup", new[] { "Strange Note" } },
+            { "Melon", new[] { "Crop Research" } },
+            { "Pale Ale", new[] { "Pam Is Thirsty" } },
+            { "Pufferfish", new[] { "Aquatic Research" } },
+            { "Pumpkin", new[] { "Carving Pumpkins" } },
+            { "Rainbow Shell", new[] { "The Mysterious Qi" } },
+            { "Sashimi", new[] { "Pierre's Notice" } },
+            { "Solar Essence", new[] { "The Mysterious Qi" } },
+            { "Starfruit", new[] { "A Soldier's Star" } },
+            { "Truffle Oil", new[] { "Mayor's Need" } },
+            { "Void Essence", new[] { "A Dark Reagent" } },
+            { "Void Mayonnaise", new[] { "Goblin Problem" } },
+        };
     }
 }
