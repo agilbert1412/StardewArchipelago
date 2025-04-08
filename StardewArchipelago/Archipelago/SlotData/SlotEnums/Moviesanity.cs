d62d99e223ce6b9b4e557e29d9195d03b043cdb2
Alex Gilbert <alexgilbert@yahoo.com>
2025-04-07 21:53:12 -0400

    - Add moviesanity enum

---
+++ StardewArchipelago/Archipelago/SlotData/SlotEnums/Moviesanity.cs
@@ -0,0 +1,13 @@
+﻿namespace StardewArchipelago.Archipelago.SlotData.SlotEnums
+{
+    public enum Moviesanity
+    {
+        None = 0,
+        One = 1,
+        AllMovies = 2,
+        AllMoviesLoved = 3,
+        AllMoviesWithLovedSnack = 4,
+        AllMoviesAndAllSnacks = 5,
+        AllMoviesAndAllLovedSnacks = 6,
+    }
+}
