c69a323b1fda4e75793d65a37fd39688bff865c6
ahhh-reptar <87342843+ahhh-reptar@users.noreply.github.com>
2025-05-23 21:04:22 -0600

    fixed identical namespace issues

--- StardewArchipelago/Constants/Modded/ModBookIds.cs
+++ StardewArchipelago/Constants/Modded/ModBookIds.cs
@@ -1,10 +1,8 @@
-using StardewArchipelago.Constants.Vanilla;
-using System.Collections.Generic;
-
 namespace StardewArchipelago.Constants.Modded
 {
     public static class ModBookIds
     {
-        public const string DIGGING_LIKE_WORMS = "moonslime.Archaeology.skill_book";
+        public const string DIGGING_LIKE_WORMS_ID = "moonslime.Archaeology.skill_book";
+        public const string DIGGING_LIKE_WORMS_NAME = "Digging Like Worms";
     }
 }
