1d5965c238e5ea4dee2f8ffa5d457dd27e9cb24b
Alex Gilbert <alexgilbert@yahoo.com>
2025-06-18 17:45:43 -0400

    - remove walnut check method from interface

--- StardewArchipelago/Locations/ILocationChecker.cs
+++ StardewArchipelago/Locations/ILocationChecker.cs
@@ -2,7 +2,6 @@
 {
     public interface ILocationChecker
     {
-        void AddWalnutCheckedLocation(string locationName);
         void AddCheckedLocations(string[] locationNames);
         void AddCheckedLocation(string locationName);
     }
