d281412c1dcbdb2c50ccff6b1b698f35f2d05f77
Alex Gilbert <alexgilbert@yahoo.com>
2025-07-02 00:08:42 -0400

    - Add many indicator exceptions

--- StardewArchipelago/Constants/Locations/NameMatching.cs
+++ StardewArchipelago/Constants/Locations/NameMatching.cs
@@ -1,6 +1,7 @@
 ﻿using StardewArchipelago.Locations.Festival;
 using StardewArchipelago.Locations.GingerIsland.Parrots;
 using System.Collections.Generic;
+using StardewArchipelago.Locations.Secrets;
 
 namespace StardewArchipelago.Constants.Locations
 {
@@ -9,13 +10,17 @@ namespace StardewArchipelago.Constants.Locations
         public static readonly Dictionary<string, string[]> Exceptions = new()
         {
             { "Anchor", new[] { "Boat Anchor" } },
+            { "Bomb", new[] { "Cherry Bomb", "Mega Bomb" } },
             { "Carp", new[] { "Midnight Carp", "Scorpion Carp", "Mutant Carp" } },
             { "Chest", new[] { "Stone Chest", "Treasure Chest", "Big Chest", "Common Chest", "Rare Chest" } },
             { "Clay", new[] { "Land Of Clay" } },
+            { "Coffee", new[] { "Coffee Bean" } },
             { "Diamond", new[] { "Diamond Hunter", "Starfish Diamond", "Diamond Of", } },
             { "Egg (Brown)", new[] { "Large Egg (Brown)" } },
-            { "Egg", new[] { "Duck Egg", "Ostrich Egg", "Dinosaur Egg", "Void Egg", "Thunder Egg", "Large Egg", "Egg (Brown)", "Brown Egg", "Slime Egg", "Calico Egg", "Egg Festival: Strawberry Seeds", "Egg Hunt Victory" } },
-            { "Hardwood", new[] { "Hardwood Display" } },
+            { "Egg", new[] { "Duck Egg", "Ostrich Egg", "Dinosaur Egg", "Void Egg", "Thunder Egg", "Large Egg", "Egg (Brown)", "Brown Egg", "Slime Egg", "Golden Egg", "Calico Egg", "Egg Festival: Strawberry Seeds", "Egg Hunt Victory" } },
+            { "Fiber", new[] { "Fiber Seeds" } },
+            { "Geode", new[] { "Geode Crusher" } },
+            { "Hardwood", new[] { "Hardwood Display", "Hardwood Fence" } },
             { "Jasper", new[] { "by M. Jasper" } },
             { "Large Egg", new[] { "Large Egg (Brown)" } },
             { "Legend", new[] { "The Legend of the Winter Star" } },
@@ -24,13 +29,16 @@ namespace StardewArchipelago.Constants.Locations
             { "Salad", new[] { "Sour Salad" } },
             { "Salmon", new[] { "Void Salmon", "King Salmon" } },
             { "Shrimp", new[] { "Rainforest Shrimp", "Shrimp Donut" } },
-            { "Slime", new[] { "Slime Hutch" } },
+            { "Slime", new[] { "Slime Hutch", "Slime Egg", "Slime Incubator" } },
             { "Snail", new[] { IslandNorthInjections.AP_PROF_SNAIL_CAVE } },
+            { "Speed-Gro", new[] { "Deluxe Speed-Gro", "Hyper Speed-Gro" } },
+            { "Sprinkler", new[] { "Quality Sprinkler", "Iridium Sprinkler" } },
             { "Stone Chest", new[] { "Big Stone Chest" } },
-            { "Stone", new[] { FestivalLocationNames.STRENGTH_GAME, "Lemon Stone", "Ocean Stone", "Fairy Stone", "Swirl Stone", } },
+            { "Stone", new[] { FestivalLocationNames.STRENGTH_GAME, "Lemon Stone", "Ocean Stone", "Fairy Stone", "Swirl Stone", "Stone Junimo", "Stone Chest", "Stone Walkway", "Stone Brazier", "Stone Sign" } },
             { "Strawberry", new[] { "Egg Festival: Strawberry Seeds" } },
-            { "Trash", new[] { "Trash Can Upgrade" } },
+            { "Trash", new[] { "Trash Can Upgrade", "Trash Bear", "Trash Catalogue" } },
             { "Turnip", new[] { "Rarecrow #1 (Turnip Head)" } },
+            { "Wood", new[] { "Wood Fence", "Wood Floor", "Wood Path", "Wood Lamp", "Wood Sign" } }
         };
     }
 }
