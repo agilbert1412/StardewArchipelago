3396da6b77772ddb78a5ad7a4baf17b93a9dc89a
Alex Gilbert <alexgilbert@yahoo.com>
2025-07-13 12:34:33 -0400

    - Removed shop recipes from chefsanity recipes that should not be in shops

--- StardewArchipelago/Locations/ShopStockModifiers/CookingRecipePurchaseStockModifier.cs
+++ StardewArchipelago/Locations/ShopStockModifiers/CookingRecipePurchaseStockModifier.cs
@@ -8,6 +8,7 @@ using StardewValley.GameData.Shops;
 using KaitoKid.ArchipelagoUtilities.Net.Interfaces;
 using StardewArchipelago.Archipelago;
 using StardewArchipelago.Archipelago.SlotData.SlotEnums;
+using StardewArchipelago.Archipelago.ApworldData;
 
 namespace StardewArchipelago.Locations.ShopStockModifiers
 {
@@ -19,11 +20,6 @@ namespace StardewArchipelago.Locations.ShopStockModifiers
 
         public override void OnShopStockRequested(object sender, AssetRequestedEventArgs e)
         {
-            if (!_archipelago.SlotData.Chefsanity.HasFlag(Chefsanity.Purchases))
-            {
-                return;
-            }
-
             if (!AssetIsShops(e))
             {
                 return;
@@ -35,6 +31,7 @@ namespace StardewArchipelago.Locations.ShopStockModifiers
 
                     foreach (var (shopId, shopData) in shopsData)
                     {
+                        RemoveFriendshipRecipesFromShops(shopId, shopData);
                         ReplaceCookingRecipesWithChefsanityChecks(shopId, shopData);
                     }
                 },
@@ -44,6 +41,11 @@ namespace StardewArchipelago.Locations.ShopStockModifiers
 
         private void ReplaceCookingRecipesWithChefsanityChecks(string shopId, ShopData shopData)
         {
+            if (!_archipelago.SlotData.Chefsanity.HasFlag(Chefsanity.Purchases))
+            {
+                return;
+            }
+
             for (var i = shopData.Items.Count - 1; i >= 0; i--)
             {
                 var item = shopData.Items[i];
@@ -63,6 +65,31 @@ namespace StardewArchipelago.Locations.ShopStockModifiers
                 var apShopItem = CreateArchipelagoLocation(item, location);
                 shopData.Items.RemoveAt(i);
                 shopData.Items.Insert(i, apShopItem);
+            }
+        }
+
+        private void RemoveFriendshipRecipesFromShops(string shopId, ShopData shopData)
+        {
+            for (var i = shopData.Items.Count - 1; i >= 0; i--)
+            {
+                var item = shopData.Items[i];
+
+                if (!item.IsRecipe)
+                {
+                    continue;
+                }
+
+                var stardewItem = _stardewItemManager.GetItemByQualifiedId(item.ItemId) ?? _stardewItemManager.GetItemByQualifiedId(QualifiedItemIds.QualifiedObjectId(item.ItemId));
+                var location = $"{stardewItem.Name}{Suffix.CHEFSANITY}";
+                if (!_archipelago.LocationExists(location))
+                {
+                    continue;
+                }
+
+                if (!_archipelago.DataPackageCache.GetLocation(location).LocationTags.Contains(LocationTag.CHEFSANITY_PURCHASE))
+                {
+                    shopData.Items.RemoveAt(i);
+                }
             }
         }
     }
