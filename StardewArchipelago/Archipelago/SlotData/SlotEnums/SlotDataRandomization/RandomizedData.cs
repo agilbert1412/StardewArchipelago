a35e9a461b8d9c62a9adc0d2e4dd4e6d6231056b
Alex Gilbert <alexgilbert@yahoo.com>
2026-04-29 18:51:50 -0400

    - Started WIP for randomized shop data

--- StardewArchipelago/Archipelago/SlotData/SlotEnums/SlotDataRandomization/RandomizedData.cs
+++ StardewArchipelago/Archipelago/SlotData/SlotEnums/SlotDataRandomization/RandomizedData.cs
@@ -1,4 +1,8 @@
 ﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using Newtonsoft.Json;
 using StardewValley;
 
 namespace StardewArchipelago.Archipelago.SlotData.SlotEnums.SlotDataRandomization
@@ -8,12 +12,14 @@ namespace StardewArchipelago.Archipelago.SlotData.SlotEnums.SlotDataRandomizatio
         public Dictionary<string, RandomizedCropData> Crops { get; set; }
         public Dictionary<string, RandomizedFishData> Fish { get; set; }
         public Dictionary<string, RandomizedFestivalData> Festivals { get; set; }
+        public Dictionary<string, Dictionary<string, RandomizedShopItemData>> Shops { get; set; }
 
         public RandomizedData()
         {
             Crops = new Dictionary<string, RandomizedCropData>();
             Fish = new Dictionary<string, RandomizedFishData>();
             Festivals = new Dictionary<string, RandomizedFestivalData>();
+            Shops = new Dictionary<string, Dictionary<string, RandomizedShopItemData>>();
         }
 
         public void AssignNames()
@@ -21,6 +27,9 @@ namespace StardewArchipelago.Archipelago.SlotData.SlotEnums.SlotDataRandomizatio
             AssignCropNames();
             AssignFishNames();
             AssignFestivalNames();
+            AssignShopNames();
+
+            ValidateData();
         }
 
         private void AssignCropNames()
@@ -45,6 +54,98 @@ namespace StardewArchipelago.Archipelago.SlotData.SlotEnums.SlotDataRandomizatio
             {
                 data.AssignName(name);
             }
+        }
+
+        private void AssignShopNames()
+        {
+            foreach (var (shopName, shopData) in Shops)
+            {
+                foreach (var (itemName, shopItemData) in shopData)
+                {
+                    shopItemData.AssignNamesAndDefaults(shopName, itemName);
+                }
+            }
+        }
+
+        private void ValidateData()
+        {
+            var allShopNames = GetAllShopNames();
+            var allItemEntries = GetAllItemEntries();
+            var allCurrencies = GetAllCurrencies();
+            var allMaterialEntries = GetAllMaterialEntries();
+
+            ExportToJson("DR - ShopNames.json", allShopNames);
+            ExportToJson("DR - Items.json", allItemEntries);
+            ExportToJson("DR - Currencies.json", allCurrencies);
+            ExportToJson("DR - Materials.json", allMaterialEntries);
+        }
+
+        private List<string> GetAllShopNames()
+        {
+            return Shops.Keys.ToHashSet().ToList();
+        }
+
+        private Dictionary<string, int> GetAllItemEntries()
+        {
+            var itemEntries = new Dictionary<string, int>();
+            foreach (var (shopName, shopData) in Shops)
+            {
+                foreach (var (itemName, shopItemData) in shopData)
+                {
+                    if (!itemEntries.ContainsKey(itemName))
+                    {
+                        itemEntries.Add(itemName, 0);
+                    }
+                    itemEntries[itemName]++;
+                }
+            }
+
+            return itemEntries;
+        }
+
+        private List<string> GetAllCurrencies()
+        {
+            var currencies = new HashSet<string>();
+            foreach (var (shopName, shopData) in Shops)
+            {
+                foreach (var (itemName, shopItemData) in shopData)
+                {
+                    currencies.Add(shopItemData.Currency);
+                }
+            }
+
+            return currencies.ToList();
+        }
+
+        private Dictionary<string, List<int>> GetAllMaterialEntries()
+        {
+            var materialEntries = new Dictionary<string, List<int>>();
+            foreach (var (shopName, shopData) in Shops)
+            {
+                foreach (var (itemName, shopItemData) in shopData)
+                {
+                    if (shopItemData?.Materials == null)
+                    {
+                        continue;
+                    }
+                    foreach (var (materialItem, materialAmount) in shopItemData.Materials)
+                    {
+                        if (!materialEntries.ContainsKey(materialItem))
+                        {
+                            materialEntries.Add(materialItem, new List<int>());
+                        }
+                        materialEntries[materialItem].Add(materialAmount);
+                    }
+                }
+            }
+
+            return materialEntries;
+        }
+
+        private void ExportToJson(string jsonPath, object data)
+        {
+            var jsonText = JsonConvert.SerializeObject(data, Formatting.Indented);
+            File.WriteAllText(jsonPath, jsonText);
         }
     }
 }
---
+++ StardewArchipelago/Archipelago/SlotData/SlotEnums/SlotDataRandomization/RandomizedShopItemData.cs
@@ -0,0 +1,35 @@
+﻿using StardewValley;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using StardewArchipelago.Bundles;
+
+namespace StardewArchipelago.Archipelago.SlotData.SlotEnums.SlotDataRandomization
+{
+    public class RandomizedShopItemData
+    {
+        public string ShopName { get; set; }
+        public string ItemName { get; set; }
+        public string Currency { get; set; }
+        public int? Price { get; set; }
+        public Dictionary<string, int> Materials { get; set; }
+
+        public RandomizedShopItemData()
+        {
+
+        }
+
+        public void AssignNamesAndDefaults(string shopName, string itemName)
+        {
+            ShopName = shopName;
+            ItemName = itemName;
+
+            if (Materials is { Count: >= 1 } && Currency == null && Price is null or <= 0)
+            {
+                Currency = "Money";
+                Price = 0;
+            }
+        }
+    }
+}
