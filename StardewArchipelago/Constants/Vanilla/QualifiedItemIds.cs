876fb0ad26770383ebd9e48242ae0fb9dcd4679f
Alex Gilbert <alexgilbert@yahoo.com>
2026-08-07 19:58:11 -0400

    - Fixed blueberry tart having a pants qualifier (breaks the display in the lingo bundle

--- StardewArchipelago/Constants/Vanilla/QualifiedItemIds.cs
+++ StardewArchipelago/Constants/Vanilla/QualifiedItemIds.cs
@@ -60,7 +60,7 @@ namespace StardewArchipelago.Constants.Vanilla
         public static readonly string BLUE_COWBOY_HAT = QualifiedHatId(HatIds.BLUE_COWBOY_HAT);
         public static readonly string BLUEBERRY = QualifiedObjectId(ObjectIds.BLUEBERRY);
         public static readonly string BLUEBERRY_SEEDS = QualifiedObjectId(ObjectIds.BLUEBERRY_SEEDS);
-        public static readonly string BLUEBERRY_TART = QualifiedPantsId(ObjectIds.BLUEBERRY_TART);
+        public static readonly string BLUEBERRY_TART = QualifiedObjectId(ObjectIds.BLUEBERRY_TART);
         public static readonly string BOAT = QualifiedFurnitureId(FurnitureIds.BOAT);
         public static readonly string BOK_CHOY_SEEDS = QualifiedObjectId(ObjectIds.BOK_CHOY_SEEDS);
         public static readonly string BOMB = QualifiedObjectId(ObjectIds.BOMB);
